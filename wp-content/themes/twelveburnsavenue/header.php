b303b9999e581a3dfb9b83336575505b8292338f
Kevin Lim <zzkevinlim@gmail.com>
2021-06-24 17:09:42 +1200

    202106241709
    Took 8 minutes

--- wp-content/themes/twelveburnsavenue/header.php
+++ wp-content/themes/twelveburnsavenue/header.php
@@ -4,6 +4,10 @@
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta property='og:title' content='<?php echo get_bloginfo( 'name' ); ?>'/>
+    <meta property='og:image' content='<?php echo get_template_directory_uri() . '/resources/assets/images/Asset 4.png'; ?>'/>
+    <meta property='og:description' content='<?php echo get_bloginfo( 'description' ); ?>'/>
+    <meta property='og:url' content='<?php echo get_bloginfo( 'wpurl' ); ?>'/>
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
